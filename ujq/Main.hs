f0335812c0b8b642e1208541ffd4465adebcaada
Michael Färber <01mf02@gmail.com>
2026-01-15 17:22:22 +0100

    Print outputs like `jaq -c`.

--- ujq/Main.hs
+++ ujq/Main.hs
@@ -6,11 +6,19 @@ import Eval (Ctx(..), run, builtins)
 main :: IO ()
 main = do
   stdin <- getContents
-  print stdin
+  --print stdin
   let tm = read stdin
-  print tm
+  --print tm
   let f = IR.compile 0 tm
   let c = Ctx {vars = Map.empty, funs = builtins, lbls = Map.empty}
   let v = Val.newVal Val.Null
-  print f
-  print $ map (fmap Val.val) $ run f c v
+  --print f
+  err <- printUntilError $ map (fmap Val.val) $ run f c v
+  maybe (return ()) (\e -> print $ "Error: " ++ show e) err
+
+printUntilError :: Show x => [Either e x] -> IO (Maybe e)
+printUntilError [] = return Nothing
+printUntilError (y:ys) =
+  case y of
+    Right x -> print x >> printUntilError ys
+    Left e -> return (Just e)
